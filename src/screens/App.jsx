3c559e58c467db94a2b713f4836824aa79639b35
c3kavi01 <c3kavi01@students.oamk.fi>
2025-09-19 18:26:11 +0300

    Navbar melkein valmis

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+//import ProtectedRoute from '../assets/components/ProtectedRoute.jsx';
+
 import Navbar from '../assets/components/Navbar.jsx';
 import Header from '../assets/components/Header.jsx';
+
+//import MyProfile from "../screens/MyProfile.jsx"
+//import Group from "../screens/Group.jsx"
 
 import Search from '../assets/components/Search.jsx'
 import SignIn from '../screens/SignIn.jsx'
@@ -25,9 +30,16 @@ function App() {
                         </>
                     }
                 />
+
+                {/*Julkiset linkit*/}
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
 
+                {/*Suojatut linkit*/}
+
+                {/*    
+                <Route path="*" element={<NotFound />} />
+                */}
             </Routes>
 
         </div>
