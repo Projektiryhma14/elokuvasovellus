7bad30c24e4938d7575eb1a3fd8c95452c490c21
Tikkanti <timo.tikkanen@hotmail.com>
2025-09-21 21:30:29 +0300

    Tilin poisto toimii profiilisivulta

--- src/screens/MyProfile.jsx
+++ src/screens/MyProfile.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import axios from 'axios'
+import { useAuth} from "../context/AuthContext.jsx"
+import {useNavigate} from 'react-router-dom'
 
 export default function MyProfile() {
+
+    const navigate = useNavigate()
+
+    const {username, token, signOut} = useAuth();
+
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+    const deleteUser = async () => {
+        try {
+            const userId = sessionStorage.getItem("user_id");
+            const response = await axios.delete(`${API_BASE_URL}/deleteuser/${userId}`)
+            alert("Käyttäjä poistettu")
+            signOut()
+            navigate("/", { replace: true, state: { flash: "User has been deleted", from: "profile" } });
+            
+            
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
+
+    
     return (
-        <div>MyProfile</div>
+        <div>
+            <div>
+                <p>My profile</p>
+                <p>Käyttäjä: {username}</p>
+            </div>
+            <button type="button" id="delete_account" onClick={() => deleteUser()}>Delete account</button>
+        </div>
     )
 }
