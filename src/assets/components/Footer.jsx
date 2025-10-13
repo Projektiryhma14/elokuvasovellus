6553c0ec5f3129cc2fdb22bef3aa827e2765bf10
c3kavi01 <c3kavi01@students.oamk.fi>
2025-10-13 17:04:08 +0300

    css-tiedostojen viimeistely

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import gitlogo from '../../pics/github-mark.png'
+import styles from "./Footer.module.css"
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+
+            <div className={styles.headline}>
+                <p>MovieGeegs</p>
+            </div>
+            <div className={styles.project_name}>
+                <p className={styles.firstPara}>Movie App, Web Programming App Project TVT24KMO</p>
+                <p>Made By: Timo, Mikko, Ville & Sari</p>
+            </div>
+            <div className={styles.repoAddress}>
+                <ul>
+                    <li><a href="https://github.com/Projektiryhma14/elokuvasovellus" target="_blank">Project repository</a></li>
+                </ul>
+            </div>
+
+
+
+        </footer >
+    )
+}
