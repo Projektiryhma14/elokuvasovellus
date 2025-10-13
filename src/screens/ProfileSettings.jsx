cbcf77fb151a46a901b8efa3c5b48f0562d1027c
Mikko Haapea <mikkohaapea@hotmail.com>
2025-10-13 18:53:57 +0300

    typo profilesettings.jsx:ssä

--- src/screens/ProfileSettings.jsx
+++ src/screens/ProfileSettings.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import styles from './profileSettings.module.css'
+import styles from './ProfileSettings.module.css'
 import { useAuth } from "../context/AuthContext.jsx"
 import { useNavigate } from 'react-router-dom'
 import { useMemo } from "react"
